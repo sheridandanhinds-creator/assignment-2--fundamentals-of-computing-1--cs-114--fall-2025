7db6a328560586dc4e53100ecab8fd552fe1b717
sheridandanhinds-creator <sheridandan.hinds@gmail.com>
2025-10-14 11:48:01 -0400

    Update CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,19 @@
 public class CountFlips {
   public static void main(String[] args) {
+    Coin dime = new Coin();
+    int headsCounter = 0;
+    int tailsCounter = 0;
+
+    for(int x = 0; x < 100; x++){
+      dime.flip();
+      if(dime.isHeads()){
+        headsCounter++;
+      }
+      else{
+        tailsCounter++;
+      }
+    }
+    System.out.println("The dime landed on heads: " + headsCounter + " times");
+    System.out.println("The dime landed on heads: " + tailsCounter + " times");
   }
 }
