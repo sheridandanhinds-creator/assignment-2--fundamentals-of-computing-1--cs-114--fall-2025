9ab14f8df3a3f963e8ff8008c2b91cad9aedc902
sheridandanhinds-creator <sheridandan.hinds@gmail.com>
2025-10-14 11:48:11 -0400

    Update OneHundredBottlesOfBeer.java

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,19 @@
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    for(int x = 100; x > 0; x--){
+      String bottleType;
+      if(x == 1) {
+        bottleType = "bottle";
+      }
+      else{
+        bottleType = "bottles"; 
+      }
+
+    System.out.println(x + " " + bottleType + " of beer on the wall ");
+    System.out.println(x + " " + bottleType + " of beer ");
+    System.out.println("If one of those bottles should happen to fall");
+    System.out.println((x - 1) + " bottles of beer on the wall\n ");
+
+    }
   }
 }
