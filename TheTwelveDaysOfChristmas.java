fc6187c6ee875f7a6612b5682493d47064bb06ec
sheridandanhinds-creator <sheridandan.hinds@gmail.com>
2025-10-14 18:40:47 -0400

    Update TheTwelveDaysOfChristmas.java

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,7 +1,7 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
     String suffix = " ";
-    for(int day = 1; day < 12; day++){
+    for(int day = 1; day <= 12; day++){
       switch (day)
       {
       case 1: 
@@ -19,55 +19,44 @@ public class TheTwelveDaysOfChristmas {
       default:
       suffix = "th";
     }
-
+     System.out.println("On the " + day + suffix + " day of Christmas my true love gave to me");
      switch (day)
       {
-      case Value1
-      //Statement to execute if Expression == Value
-      break;
+      case 12:
+      System.out.println("Twelve drummers drumming,");
 
-      case Value2
-      //Statement to execute if Expression == Value
-      break;
+      case 11:
+      System.out.println("Eleven pipers piping,");
 
-      case Value3
-      //Statement to execute if Expression == Value
-      break;
+      case 10:
+      System.out.println("Ten lords a-leaping,");
 
-      case Value4
-      //Statement to execute if Expression == Value
-      break;
+      case 9:
+      System.out.println("Nine ladies dancing,");
 
-      case Value5
-      //Statement to execute if Expression == Value
-      break;
+      case 8:
+      System.out.println("Eight maids a-milking,");
 
-      case Value6
-      //Statement to execute if Expression == Value
-      break;
+      case 7:
+      System.out.println("Seven swans a-swimming,");
 
-      case Value7
-      //Statement to execute if Expression == Value
-      break;
+      case 6:
+      System.out.println("Six geese a-laying,");
 
-      case Value8
-      //Statement to execute if Expression == Value
-      break;
+      case 5:
+      System.out.println("Five golden rings,");
 
-      case Value9
-      //Statement to execute if Expression == Value
-      break;
+      case 4:
+      System.out.println("Four calling birds,");
 
-      case Value10
-      //Statement to execute if Expression == Value
-      break;
+      case 3:
+      System.out.println("Three French hens,");
 
-      case Value11
-      //Statement to execute if Expression == Value
-      break;
+      case 2:
+      System.out.println("Two turtle doves, and");
 
       default:
-      //Statement to execute if no case matches
+      System.out.println("A partridge in a pear tree\n");
       break;
       }
     }
